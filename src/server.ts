4f75757ad056cf9066d8fbda7fc8fd2d9d81d1ce
Dmytrii Tsybuliak <tonistark1997@gmail.com>
2025-08-10 22:32:39 +0300

    strict for requests

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { env } from './utils/env';
 import { pinoHttp } from 'pino-http';
 import router from './routers';
+import { errorHandler } from './middlewares/errorHandler';
 
 const PORT = Number(env('PORT', '6000'));
 export const setupServer = () => {
@@ -16,10 +17,17 @@ export const setupServer = () => {
       },
     })
   );
-  // Example route
+
   app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Server works');
   });
+
+  app.use((req, res) => {
+    res.status(404).json({ message: 'Route not found' });
+  });
+
+  app.use(errorHandler);
+
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
--- src/validation/task.ts
+++ src/validation/task.ts
@@ -1,22 +1,24 @@
 import z from 'zod';
 
-const TaskSchema = z.object({
-  title: z.string().min(1, 'Title is required'),
-  description: z.string().optional(),
-  dueDate: z.date().optional(),
-  priority: z.enum(['low', 'medium', 'high']).default('medium'),
-  isCompleted: z.boolean().default(false),
-  tags: z.array(z.string()).optional(),
-  subtasks: z
-    .array(
-      z.object({
-        title: z.string().min(1, 'Subtask title is required'),
-        status: z.enum(['todo', 'done']).default('todo'),
-      })
-    )
-    .optional(),
-  userId: z.uuid('Invalid user ID format').optional(),
-});
+const TaskSchema = z
+  .object({
+    title: z.string().min(1, 'Title is required'),
+    description: z.string().optional(),
+    dueDate: z.date().optional(),
+    priority: z.enum(['low', 'medium', 'high']).default('medium'),
+    isCompleted: z.boolean().default(false),
+    tags: z.array(z.string()).optional(),
+    subtasks: z
+      .array(
+        z.object({
+          title: z.string().min(1, 'Subtask title is required'),
+          status: z.enum(['todo', 'done']).default('todo'),
+        })
+      )
+      .optional(),
+    userId: z.uuid('Invalid user ID format').optional(),
+  })
+  .strict();
 
 export type Task = z.infer<typeof TaskSchema>;
 
